130a8f791b4ed5e5c1f7718a81b7a89484671692
rvilchesRA <rvilches@rosaagustina.cl>
2022-10-01 12:38:11 -0300

    TM-4 ControllerMedicamentos

---
+++ BackEndDronMedicamento/Controllers/MedicamentosController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BackEndDronMedicamento;
+using BackEndDronMedicamento.Entidades;
+using System.Text.RegularExpressions;
+
+namespace BackEndDronMedicamento.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicamentosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MedicamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Medicamentos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Medicamento>>> GetMedicamentos()
+        {
+            return await _context.Medicamentos.ToListAsync();
+        }
+
+        // GET: api/Medicamentos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Medicamento>> GetMedicamento(string id)
+        {
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+
+            return medicamento;
+        }
+
+        // PUT: api/Medicamentos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMedicamento(string id, Medicamento medicamento)
+        {
+            if (id != medicamento.Codigo)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(medicamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MedicamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Medicamentos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Medicamento>> PostMedicamento(Medicamento medicamento)
+        {
+            //expresiones regulares de validación.
+            Regex regexNombre = new Regex("^[a-zA-Z0-9_-]+$");
+            Regex regexCodigo = new Regex("^[A-Z0-9_]+$");
+            
+            if (regexNombre.IsMatch(medicamento.Nombre))
+            {
+                Ok("Correcto");
+            }
+            else
+            {
+                return NotFound("El nombre solo puede contener letras, numeros, guion y guion bajo");
+            }
+            if (regexCodigo.IsMatch(medicamento.Codigo))
+            {
+                Ok("Correcto");
+            }
+            else
+            {
+                return NotFound("El codigo solo puede contener Mayusculas, numeros y guion bajo.");
+            }
+
+
+            _context.Medicamentos.Add(medicamento);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MedicamentoExists(medicamento.Codigo))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMedicamento", new { id = medicamento.Codigo }, medicamento);
+        }
+
+        // DELETE: api/Medicamentos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMedicamento(string id)
+        {
+            var medicamento = await _context.Medicamentos.FindAsync(id);
+            if (medicamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Medicamentos.Remove(medicamento);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MedicamentoExists(string id)
+        {
+            return _context.Medicamentos.Any(e => e.Codigo == id);
+        }
+    }
+}
